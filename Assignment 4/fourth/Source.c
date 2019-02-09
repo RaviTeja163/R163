ddf8bdc5ed657ccaf5337c26d2aca9296f85fb0d
cdac <cdac@DESKTOP-SS5H7HT>
2019-02-08 16:29:33 -0800

    asst4 4q

---
+++ Assignment 4/fourth/Source.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+	char *cat = { "The cat sat" };
+	int n = replace(cat);
+}
--- Assignment 4/third/Source.c
+++ Assignment 4/third/Source.c
@@ -2,5 +2,21 @@
 int main()
 {
 	char * s[] = { "we will teach you how to ", "Move a mountain ", "Level a building ", "Erase the past ", "Make a million " };
-	for(int i=0;)
+	int c = 0,d=0;
+	int len = sizeof(s) / sizeof(s[0]);
+	//printf("%d\n", len);
+	for (int i = 0; i < len; i++)
+	{
+		for (int j = 0; s[i][j] != 0; j++)
+		{
+			if ((s[i][j] == 'e') || (s[i][j] == 'E'))
+				c++;
+			if (s[i][j] == 'e')
+				d++;
+		}
+	}
+	printf("No of E and e = %d\n", c);
+	printf("No of e = %d\n", d);
+	getchar();
+	return 0;
 }
