49418accc16775485991e78bda002070d4a2a3c7
RaviTeja163 <47236746+RaviTeja163@users.noreply.github.com>
2019-02-21 11:33:10 +0530

    a

--- Assignment 4/tenth/Source.c
+++ Assignment 4/tenth/Source.c
@@ -1,4 +1,31 @@
 #include<stdio.h>
 #include<string.h>
-
+#include<stdlib.h>
+void delete(char s[],char c)
+{
+	int i,j=0;
+	int len=strlen(s);
+	char c1;
+	char s1[20];
+	for(i=0;i<len;i++)
+	{
+		if(s[i]!=c)
+		{
+			c1=s[i];
+			s1[j++]=c1;
+		}
+	}
+	s1[j]='\0';
+	printf("Corrected string is : %s\n",s1);
+}
 int main()
+{
+	char s[20];
+	char c;
+	printf("Enter the string\n");
+	gets(s);
+	printf("Enter the character to be deleted\n");
+	scanf("%c",&c);
+	delete(s,c);
+	return 0;
+}
