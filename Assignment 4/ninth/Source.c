970b91d89e78a7e771071dec8c735927106995f9
RaviTeja163 <47236746+RaviTeja163@users.noreply.github.com>
2019-02-21 11:32:30 +0530

    a

--- Assignment 4/ninth/Source.c
+++ Assignment 4/ninth/Source.c
@@ -1,4 +1,46 @@
 #include<stdio.h>
 #include<string.h>
-
+#include<stdlib.h>
+void any(char *s1,char *s2)
+{
+	char *p1,*p2,*p3;
+	int i,j,flag=0;
+	p1=s1;
+	p2=s2;
+	for(i=0;i<strlen(s1);i++)
+	{
+		if(*p1==*p2)
+		{
+			p3=p1;
+			for(j=0;j<strlen(s2);j++)
+			{
+				if(*p3==*p2)
+				{
+					p3++;
+					p2++;
+				}
+				else
+					break;
+			}
+			p2=s2;
+			if(j==strlen(s2))
+			{
+				flag=1;
+				printf("Substring is found\n");
+			}
+		}
+		p1++;
+	}
+	if(flag==0)
+		printf("Substring not found\n");
+}
 int main()
+{
+	char string[30],sub[10];
+	printf("Enter the string\n");
+	gets(string);
+	printf("Enter the substring\n");
+	gets(sub);
+	any(string,sub);
+	return 0;
+}	
