04e311012490ea4373ff30502da5ae2972706210
RaviTeja163 <47236746+RaviTeja163@users.noreply.github.com>
2019-02-21 11:31:55 +0530

    a

--- Assignment 4/eighth/Source.c
+++ Assignment 4/eighth/Source.c
@@ -1,4 +1,34 @@
 #include<stdio.h>
 #include<string.h>
-
+#include<stdlib.h>
 int main()
+{
+	int n,i,j;
+	printf("Enter the number of strings required:");
+	scanf("%d",&n);
+	char *ch[n];
+	for(i=0;i<n;i++)
+	{
+		printf("Enter string %d :",i+1);
+		char name[20];
+		scanf("%s",name);
+		int len=strlen(name);
+		ch[i]=(char*)malloc((len+1)*(sizeof(char)));
+		if(ch[i]==NULL)
+		{
+			return -1;
+			free(ch[i-1]);
+			for(j=i-2;j>=0;j--)
+				free(ch[j]);
+		}
+		strcpy(ch[i],name);
+	}
+	printf("The strings are:\n");
+	for(i=0;i<n;i++)
+	{
+		printf("%s\n",ch[i]);
+	}
+	for(j=n-1;j>=0;j--)
+		free(ch[j]);
+	return 0;
+}
